83e6578c1eda420e8f390c777532413ddd0378e0
aprox <giedrius.punis@gmail.com>
2024-08-13 20:48:26 +0300

    gui upd

--- App/Pages/HomePage.py
+++ App/Pages/HomePage.py
@@ -16,7 +16,7 @@ class HomePage(QWidget):
         left_widget.setFixedWidth(80)
         left_widget.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
         left_layout = QVBoxLayout(left_widget)
-        left_layout.setAlignment(Qt.AlignTop | Qt.AlignCenter)
+        left_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
         left_layout.setContentsMargins(0, 0, 0, 0)
         left_layout.setSpacing(0)
         
@@ -24,12 +24,36 @@ class HomePage(QWidget):
         center_widget = QWidget()
         center_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         center_layout = QVBoxLayout(center_widget)
-        center_layout.setAlignment(Qt.AlignTop | Qt.AlignCenter)
+        center_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
         center_layout.setContentsMargins(0, 0, 0, 0)
         center_layout.setSpacing(0)
+        
         title = QLabel("Grubber", self)
+        title.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        title.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         title.setStyleSheet("font: 64px 'Passero One'; color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0,stop: 0 #FFA400, stop: 1 #DB00FF); padding: 48px 0;")
+        
+        hero = QVBoxLayout()
+        hero.setSpacing(16)
+        hero.setContentsMargins(0, 32, 0, 32)
+        HEADLINE = 'Take youtube with you!'
+        headline = QLabel(HEADLINE, self)
+        headline.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        headline.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        headline.setStyleSheet("font: 32px 'Ubuntu Medium'; color: #FEFEFE;")
+        
+        PARAGRAPH = 'Grubber makes it easy to download high-quality YouTube videos up to 4K.\nYou can also easily trim the video to download only the parts you want!'
+        paragraph = QLabel(PARAGRAPH, self)
+        paragraph.setWordWrap(True)
+        paragraph.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        paragraph.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        paragraph.setStyleSheet("font: 16px 'Ubuntu'; color: #9F9F9F;")
+        
+        hero.addWidget(headline)
+        hero.addWidget(paragraph)
+        
         center_layout.addWidget(title)
+        center_layout.addLayout(hero)
 
         right_widget = QWidget()
         right_widget.setFixedWidth(80)
