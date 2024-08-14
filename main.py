2005114b746972c6bd4bd851f413c7cf1fb5562a
aprox <giedrius.punis@gmail.com>
2024-08-14 12:14:26 +0300

    mian refactor

--- main.py
+++ main.py
@@ -6,33 +6,29 @@ from App.Pages.HomePage import HomePage
 from App.Classes.Utility import LoadCustomFonts, ConfigManager
 from App.Classes.Router import Router
 
-class GUI(QWidget):
-    def __init__(self):
-        super().__init__()
-        
-        self.config = ConfigManager()
-        self.customFonts = LoadCustomFonts()
-        
-        self.body = QVBoxLayout()
-        self.body.setSpacing(0)
-        self.body.setContentsMargins(0, 0, 0, 0)
-        self.setFixedSize(800, 640)
-        self.setFont(QFont(self.config.font, 16))
-        self.setWindowIcon(QIcon('./assets/public/grubber-color.png'))
-        self.setWindowTitle('Grubber')
-        self.setLayout(self.body)
-        
-        self.router = Router(self.body)
-        self.router.load_initial(HomePage(self))
-
 def main():
     app = QApplication(sys.argv)
-    
+
     with open("./App/Styles.qss", "r") as f:
         _style = f.read()
         app.setStyleSheet(_style)
 
-    viewport = GUI()
+    config = ConfigManager()
+    customFonts = LoadCustomFonts()
+    customFonts.load_fonts()
+
+    viewport = QWidget()
+    viewport.setFixedSize(800, 640)
+    viewport.setFont(QFont(config.font, 16))
+    viewport.setWindowIcon(QIcon('./assets/public/grubber-color.png'))
+    viewport.setWindowTitle('Grubber')
+
+    body = QVBoxLayout(viewport)
+    body.setContentsMargins(0, 0, 0, 0)
+    body.setSpacing(0)
+    router = Router(body)
+    router.load_initial(HomePage(viewport))
+
     viewport.show()
 
     sys.exit(app.exec())
